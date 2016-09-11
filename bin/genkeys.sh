42bf2451e5e56a82d81993cdeced0f104ef021c7
kwantam <kwantam@gmail.com>
2016-09-10 18:52:01 -0700

    genkeys.sh: generate two server certs

--- bin/genkeys.sh
+++ bin/genkeys.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # generate all the keys
-for i in ca server; do
+for i in ca server server2; do
     openssl genrsa -out ${i}_key.pem 2048
 done
 
@@ -9,12 +9,16 @@ done
 openssl req -x509 -new -sha256 -nodes -key ca_key.pem -days 3650 -out ca_cert.pem -subj "/C=US/ST=CA/O=mu/CN=mu_ca"
 
 # signing request
-openssl req -new -sha256 -key server_key.pem -out server_csr.pem -subj "/C=US/ST=NY/O=mu/CN=mu_srv"
+for i in server server2; do
+    openssl req -new -sha256 -key ${i}_key.pem -out ${i}_csr.pem -subj "/C=US/ST=NY/O=mu/CN=mu_${i}"
+done
 
 # sign
-openssl x509 -req -in server_csr.pem -CA ca_cert.pem -CAkey ca_key.pem -CAcreateserial -out server_cert.pem -days 3650
+for i in server server2; do
+    openssl x509 -req -in ${i}_csr.pem -CA ca_cert.pem -CAkey ca_key.pem -CAcreateserial -out ${i}_cert.pem -days 3650
+done
 
-rm ca_cert.srl server_csr.pem
+rm ca_cert.srl server_csr.pem server2_csr.pem
 
 # now do something like
 # openssl s_server -CAfile server_chain.pem -cert server_chain.pem -key server_key.pem -debug
