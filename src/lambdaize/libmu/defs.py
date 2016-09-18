3b1cc13e0eb767e776b577f1eb494bfe74f3ee47
kwantam <kwantam@gmail.com>
2016-09-17 19:43:02 -0700

    I only called it fun so that I could say 'turn off the fun'. Turning off the fun now.

--- src/lambdaize/libmu/defs.py
+++ src/lambdaize/libmu/defs.py
@@ -6,7 +6,7 @@ class Defs(object):
     header_fmt = "%012d %s"
     cipher_list = "ECDHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-RC4-SHA:ECDHE-RSA-AES256-SHA:HIGH:!aNULL:!eNULL:!EXP:!LOW:!MEDIUM:!MD5:!RC4:!DES:!3DES"
     debug = False
-    fun = True
+    fun = False
     cmdstring = ''
 
     @staticmethod
